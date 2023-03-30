23054b0ceea5254607614693c6e0a2e374e6b16c
EdipNaser <naseredib@gmail.com>
2023-03-30 04:35:50 +0300

    All Done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"]
+const msgs = []
+
+function writeCards(names) {
+    for (let index = 0; index < names.length; index++) {
+        const msg = `Thank you, ${names[index]}, for the wonderful surprise gift!`
+        msgs.push(msg)
+    }
+    return msgs
+}
+
+let i = 0
+function countDown(number) {
+    while(i<=number) {
+        console.log(number)
+        number --
+    }
+}
